8c5582216e56ee16b93b46bbb214347d2e48fc5d
Devvrat1010 <rathoddevvratsingh777@gmail.com>
2024-04-10 23:23:02 +0530

    backend

--- frontend/src/pages/signin.jsx
+++ frontend/src/pages/signin.jsx
@@ -17,7 +17,7 @@ export default function Signup() {
     const handleSubmit = (e) => {
         e.preventDefault()
 
-        fetch('http://localhost:5000/api/auth/signin', {
+        fetch('https://aeonaxy-8u8e.onrender.com', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
